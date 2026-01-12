998f8a7c73e4fbf189218a6236ccf681431b7782
Hao Hon Yap <100479@corp.b2be.com>
2026-01-12 13:27:00 +0800

    Add End-to-End checkout test

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
-        self.username_field = (By.ID, "user-name")
+        self.username_field = (By.ID, "wrong-name")
         self.password_field = (By.ID, "password")
         self.login_button = (By.ID, "login-button")
         self.error_message = (By.CSS_SELECTOR, "h3[data-test='error']")
